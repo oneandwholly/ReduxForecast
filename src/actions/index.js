1617ee29180623179d97d6c2e1f4a244b5e5dc75
Jon Huh <oneandwholly@gmail.com>
2017-05-09 21:47:11 +0900

    fix typos in fetchWeather ActionCreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import axios form 'axios';
+import axios from 'axios';
 
-Const API_KEY = 'a4bf722123ddbb7a4a62c31726e790b8';
-Const ROOT_ULR = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = 'a4bf722123ddbb7a4a62c31726e790b8';
+const ROOT_ULR = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export default fetchWeather(city) {
+export default function fetchWeather(city) {
   const url = `${ROOT_ULR}&q=${city},us`;
   const request = axios.get(url);
 
